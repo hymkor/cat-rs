244a3f35d2c0e6b01b18a73c105a0eb8ca615aea
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-09-01 22:07:03 +0900

    function returns Result<i32,io:Error>

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
-fn copy_file_content_to_stdout(count: &mut i32, filename: &str) -> io::Result<()> {
+fn copy_file_content_to_stdout(mut count: i32, filename: &str) -> Result<i32,io::Error> {
     let file = match File::open(filename) {
         Ok(file) => file,
         Err(e) => { return Err(e) }
@@ -10,10 +10,10 @@ fn copy_file_content_to_stdout(count: &mut i32, filename: &str) -> io::Result<()
     let reader = BufReader::new(file);
 
     for line in reader.lines() {
-        *count += 1;
-        println!("{}: {}", *count,line?);
+        count += 1;
+        println!("{}: {}", count,line?);
     }
-    Ok(())
+    Ok(count)
 }
 
 fn main() {
@@ -26,9 +26,12 @@ fn main() {
 
     let mut count = 0;
     for filename in &args[1..] {
-        if let Err(err) = copy_file_content_to_stdout(&mut count,filename) {
-            eprintln!("Error: {}", err);
-            std::process::exit(1);
+        match copy_file_content_to_stdout(count,filename){
+            Ok(c) => count = c,
+            Err(err) => {
+                eprintln!("Error: {}", err);
+                std::process::exit(1);
+            }
         }
     }
 }
