78c64d8608ae925b98581168ca33a3846240804d
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-09-01 22:00:37 +0900

    Support line number

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader};
 
-fn copy_file_content_to_stdout(filename: &str) -> io::Result<()> {
+fn copy_file_content_to_stdout(count: &mut i32, filename: &str) -> io::Result<()> {
     let file = match File::open(filename) {
         Ok(file) => file,
         Err(e) => { return Err(e) }
@@ -10,7 +10,8 @@ fn copy_file_content_to_stdout(filename: &str) -> io::Result<()> {
     let reader = BufReader::new(file);
 
     for line in reader.lines() {
-        println!("{}", line?);
+        *count += 1;
+        println!("{}: {}", *count,line?);
     }
     Ok(())
 }
@@ -23,8 +24,9 @@ fn main() {
         std::process::exit(1);
     }
 
+    let mut count = 0;
     for filename in &args[1..] {
-        if let Err(err) = copy_file_content_to_stdout(filename) {
+        if let Err(err) = copy_file_content_to_stdout(&mut count,filename) {
             eprintln!("Error: {}", err);
             std::process::exit(1);
         }
