5f4ba8ad498d519134490060f21b68027bbf7ac7
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-09-06 03:23:03 +0900

    Use &mut

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::fs::File;
 use std::io::{self,BufRead};
 use std::fmt;
 
-fn cat1<R: std::io::Read>(count: Option<i32>, r: R) -> Result<Option<i32>,io::Error> {
+fn cat1<R: std::io::Read>(count: &mut Option<i32>, r: R) -> Result<(),io::Error> {
     let reader = io::BufReader::new(r);
 
     if let Some(mut c) = count {
@@ -14,13 +14,13 @@ fn cat1<R: std::io::Read>(count: Option<i32>, r: R) -> Result<Option<i32>,io::Er
             c += 1;
             println!("{}: {}", c,line?);
         }
-        Ok(Some(c))
+        *count = Some(c);
     } else {
         for line in reader.lines() {
             println!("{}", line?);
         }
-        Ok(None)
     }
+    Ok(())
 }
 
 enum CatError {
@@ -58,30 +58,30 @@ impl From<glob::PatternError> for CatError {
 }
 
 fn cat(args : Vec<String>) -> Result<(),CatError> {
+    let mut count : Option<i32> = None;
     if args.len() < 2 {
-        cat1(None,io::stdin())?;
+        cat1(&mut count,io::stdin())?;
         return Ok(())
     }
 
-    let mut count : Option<i32> = None;
     for arg in &args[1..] {
         if arg == "-n" {
             count = Some(0);
             continue;
         }
         if arg == "-" {
-            count = cat1(count,io::stdin())?;
+            cat1(&mut count,io::stdin())?;
             continue
         }
         let mut glob_ok = false;
         for filename in glob(arg)? {
             if let Some(filename) = filename?.to_str() {
-                count = cat1(count,File::open(filename)?)?;
+                cat1(&mut count,File::open(filename)?)?;
                 glob_ok = true;
             }
         }
         if ! glob_ok {
-            count = cat1(count,File::open(arg)?)?
+            cat1(&mut count,File::open(arg)?)?
         }
     }
     return Ok(())
