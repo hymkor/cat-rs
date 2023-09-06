f2445e057d07dcf0617fa8cedae253e398bb43d0
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-09-06 11:56:44 +0900

    Do not use std::env::args().collect()

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,15 @@ extern crate glob;
 use std::fs::File;
 use std::io::{self,BufRead};
 
-fn cat1<R: std::io::Read>(count: &mut Option<i32>, r: R) -> Result<(),io::Error> {
+fn cat1<R: std::io::Read>(line_count: &mut Option<i32>, r: R) -> Result<(),io::Error> {
     let reader = io::BufReader::new(r);
 
-    if let Some(mut c) = count {
+    if let Some(mut c) = line_count {
         for line in reader.lines() {
             c += 1;
             println!("{}: {}", c,line?);
         }
-        *count = Some(c);
+        *line_count = Some(c);
     } else {
         for line in reader.lines() {
             println!("{}", line?);
@@ -20,38 +20,39 @@ fn cat1<R: std::io::Read>(count: &mut Option<i32>, r: R) -> Result<(),io::Error>
     Ok(())
 }
 
-fn cat(args : Vec<String>) -> Result<(),Box<dyn std::error::Error>> {
-    let mut count : Option<i32> = None;
-    if args.len() < 2 {
-        cat1(&mut count,io::stdin())?;
-        return Ok(())
-    }
+fn cat(args: std::env::Args) -> Result<(),Box<dyn std::error::Error>> {
+    let mut file_done = false;
+    let mut line_count : Option<i32> = None;
 
-    for arg in &args[1..] {
+    for arg in args.skip(1) {
         if arg == "-n" {
-            count = Some(0);
+            line_count = Some(0);
             continue;
         }
+        file_done = true;
         if arg == "-" {
-            cat1(&mut count,io::stdin())?;
+            cat1(&mut line_count,io::stdin())?;
             continue
         }
         let mut glob_ok = false;
-        for filename in glob::glob(arg)? {
+        for filename in glob::glob(&arg)? {
             if let Some(filename) = filename?.to_str() {
-                cat1(&mut count,File::open(filename)?)?;
+                cat1(&mut line_count,File::open(filename)?)?;
                 glob_ok = true;
             }
         }
         if ! glob_ok {
-            cat1(&mut count,File::open(arg)?)?
+            cat1(&mut line_count,File::open(arg)?)?
         }
+    }
+    if ! file_done {
+        cat1(&mut line_count,io::stdin())?;
     }
     return Ok(())
 }
 
 fn main(){
-    if let Err(err) = cat(std::env::args().collect()) {
+    if let Err(err) = cat(std::env::args()) {
         eprintln!("{}",err);
         std::process::exit(1);
     }
