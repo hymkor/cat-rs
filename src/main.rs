967aaae49dc1e711c70a2f5d67176373964c1701
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-09-08 01:31:31 +0900

    Use std::io::copy

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,16 @@ extern crate glob;
 use std::fs::File;
 use std::io::{self,BufRead};
 
-fn cat1<R: std::io::Read>(line_count: &mut Option<i32>, r: R) -> Result<(),io::Error> {
-    let reader = io::BufReader::new(r);
-
+fn cat1<R: std::io::Read>(line_count: &mut Option<i32>, r: &mut R) -> Result<(),io::Error> {
     if let Some(mut c) = line_count {
+        let reader = io::BufReader::new(r);
         for line in reader.lines() {
             c += 1;
             println!("{}: {}", c,line?);
         }
         *line_count = Some(c);
     } else {
-        for line in reader.lines() {
-            println!("{}", line?);
-        }
+        std::io::copy(r,&mut std::io::stdout())?;
     }
     Ok(())
 }
@@ -31,22 +28,22 @@ fn cat(args: std::env::Args) -> Result<(),Box<dyn std::error::Error>> {
         }
         file_done = true;
         if arg == "-" {
-            cat1(&mut line_count,io::stdin())?;
+            cat1(&mut line_count,&mut io::stdin())?;
             continue
         }
         let mut glob_ok = false;
         for filename in glob::glob(&arg)? {
             if let Some(filename) = filename?.to_str() {
-                cat1(&mut line_count,File::open(filename)?)?;
+                cat1(&mut line_count,&mut File::open(filename)?)?;
                 glob_ok = true;
             }
         }
         if ! glob_ok {
-            cat1(&mut line_count,File::open(arg)?)?
+            cat1(&mut line_count,&mut File::open(arg)?)?
         }
     }
     if ! file_done {
-        cat1(&mut line_count,io::stdin())?;
+        cat1(&mut line_count,&mut io::stdin())?;
     }
     return Ok(())
 }
