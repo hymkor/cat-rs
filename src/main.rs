099a6de926e94343b314b6eae554f438c989a9cd
HAYAMA_Kaoru <iyahaya@nifty.com>
2023-09-08 19:42:31 +0900

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
 extern crate glob;
 
 use std::fs::File;
-use std::io::{self,BufRead};
+use std::io::{self, BufRead};
 
-fn cat1<R: std::io::Read>(line_count: &mut Option<i32>, r: &mut R) -> Result<(),io::Error> {
+fn cat1<R: std::io::Read>(line_count: &mut Option<i32>, r: &mut R) -> Result<(), io::Error> {
     if let Some(mut c) = line_count {
         let reader = io::BufReader::new(r);
         for line in reader.lines() {
             c += 1;
-            println!("{}: {}", c,line?);
+            println!("{}: {}", c, line?);
         }
         *line_count = Some(c);
     } else {
-        std::io::copy(r,&mut std::io::stdout())?;
+        std::io::copy(r, &mut std::io::stdout())?;
     }
     Ok(())
 }
 
-fn cat(args: std::env::Args) -> Result<(),Box<dyn std::error::Error>> {
+fn cat(args: std::env::Args) -> Result<(), Box<dyn std::error::Error>> {
     let mut file_done = false;
-    let mut line_count : Option<i32> = None;
+    let mut line_count: Option<i32> = None;
 
     for arg in args.skip(1) {
         if arg == "-n" {
@@ -28,29 +28,29 @@ fn cat(args: std::env::Args) -> Result<(),Box<dyn std::error::Error>> {
         }
         file_done = true;
         if arg == "-" {
-            cat1(&mut line_count,&mut io::stdin())?;
-            continue
+            cat1(&mut line_count, &mut io::stdin())?;
+            continue;
         }
         let mut glob_ok = false;
         for filename in glob::glob(&arg)? {
             if let Some(filename) = filename?.to_str() {
-                cat1(&mut line_count,&mut File::open(filename)?)?;
+                cat1(&mut line_count, &mut File::open(filename)?)?;
                 glob_ok = true;
             }
         }
-        if ! glob_ok {
-            cat1(&mut line_count,&mut File::open(arg)?)?
+        if !glob_ok {
+            cat1(&mut line_count, &mut File::open(arg)?)?
         }
     }
-    if ! file_done {
-        cat1(&mut line_count,&mut io::stdin())?;
+    if !file_done {
+        cat1(&mut line_count, &mut io::stdin())?;
     }
-    return Ok(())
+    return Ok(());
 }
 
-fn main(){
+fn main() {
     if let Err(err) = cat(std::env::args()) {
-        eprintln!("{}",err);
+        eprintln!("{}", err);
         std::process::exit(1);
     }
 }
